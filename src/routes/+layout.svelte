b3e78478fce48175cb987c4bc1d1925cca46bedc
Mystypix <jakub.smagin@gmail.com>
2023-03-03 08:40:56 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="navbar bg-primary">
+	<a href="/dashboard">Dashboard</a>
+	<a href="/sign-in-and-sign-up">Sign in and sign up</a>
+</nav>
+
+<div class="container">
+	<slot />
+</div>
---
+++ src/routes/sign-in-and-sign-up/+page.svelte
@@ -0,0 +1,6 @@
+<h1 class="mb-6 text-center">Sign in and sign up</h1>
+<form class="grid grid-cols1">
+	<input type="text" class="input input-bordered w-full max-w-xs mb-3" />
+	<input type="text" class="input input-bordered w-full max-w-xs mb-3" />
+	<button type="submit">Sign in</button>
+</form>
